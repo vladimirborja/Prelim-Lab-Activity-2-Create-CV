cacef8724a5be632ef53664f5ba9a5dee8572150
Vladimir Borja <vladimirborja@Vladimirs-Mac-mini.local>
2024-11-29 22:38:49 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { EducationComponent } from './education/education.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { TrainingComponent } from './training/training.component';
+import { ReferencesComponent } from './references/references.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PersonalInfoComponent, EducationComponent, WorkExperienceComponent, TrainingComponent, ReferencesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
